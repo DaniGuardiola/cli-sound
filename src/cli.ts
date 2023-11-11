6c2cba0e05a02e2777b03fca3c6b411fe09624f6
Dani Guardiola <hi@daniguardio.la>
2023-11-11 02:26:52 +0100

    Add cli-sound binary (#2)

---
+++ src/cli.ts
@@ -0,0 +1,51 @@
+import path from "node:path";
+
+import { Player } from "./player.js";
+
+const args = process.argv.slice(2);
+
+const HELP = `
+Usage: cli-sound [options] <filepath>
+
+Options:
+  -v, --volume    Set the volume (0-1).
+  -h, --help      Show this help message and exit.
+`.trim();
+
+if (args[0] === "-h" || args[0] === "--help") {
+  console.log(HELP);
+  process.exit(0);
+}
+
+let volume = 1;
+let filePath: string;
+
+if (args[0] === "-v" || args[0] === "--volume") {
+  volume = Number(args[1]);
+  filePath = args[2];
+} else {
+  filePath = args[0];
+}
+
+const absoluteFilePath = path.resolve(process.cwd(), filePath);
+
+async function main() {
+  try {
+    const player = new Player({ volume });
+
+    try {
+      console.log(`Playing ${filePath}`);
+      await player.play(absoluteFilePath);
+    } catch (error) {
+      console.error("There was an error playing the file:");
+      console.error(error);
+      process.exit(1);
+    }
+  } catch (error) {
+    console.error("There was an error creating the player:");
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+main();
